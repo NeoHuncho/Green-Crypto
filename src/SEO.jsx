d2cfbe102be674db63c24c6bcb56ea787ba2373a
Neo Huncho <william.guinaudie@gmail.com>
2022-01-24 10:40:25 +0100

    Fixes to SEO

--- src/SEO.jsx
+++ src/SEO.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Helmet from "react-helmet";
+import { black } from "./colors/colors";
 
 export default function SEO() {
   return (
     <Helmet>
+      <title>Green Crypto Ranking</title>
+      <meta
+        name="description"
+        content="Which cryptos are the most sustainable/polluting? 1st: IOTA(.01Wh), 2nd: HBAR(.02Wh), 3rd: ETH2.0(.09Wh) 4th: NANO(.11Wh) 5th: ALGO(.17Wh) 6th: XLM(.22Wh) 7th XTZ(0.36Wh) "
+      />
+      <meta name="author" content="NeoHuncho" />
+      <meta name="theme-color" content={black} />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
